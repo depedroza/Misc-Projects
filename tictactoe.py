f09d895e27c4fcfdd73c2f4734beede8601aaa40
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-05-09 00:15:32 -0500

    finished tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,10 @@
 import numpy as np
+import random
 
 
 def main():
-    board = np.empty((3, 3), dtype=str)
-    board_key = np.arange(1, 10, 1).reshape(3, 3)
+    board = np.full((3, 3), "-")
+
     isPlaying = False
 
     print(board)
@@ -22,15 +23,29 @@ def main():
         elif i.lower().startswith("y"):
             isPlaying = True
 
-    icon = input("Do you want to be Xs or Os?")
+    icon = input("Do you want to be Xs or Os?").upper()
 
     while not (icon.lower() == "x" or icon.lower() == "o"):
         print("Choose a valid icon!")
         icon = input("Do you want to be Xs or Os?")
 
+    play_game(isPlaying, icon)
+
+
+def play_game(isPlaying, icon):
+    board = np.full((3, 3), "-")
+    openslots = 9
+    display_board(board)
     while isPlaying == True:
-        display_key(board_key)
-        user_move(board_key, icon)
+        board = user_move(board, icon)
+        openslots -= 1
+        display_board(board)
+        check_winner(board, icon, openslots)
+        print("The CPU moves!")
+        board = cpu_move(board, icon)
+        openslots -= 1
+        display_board(board)
+        check_winner(board, icon, openslots)
 
 
 def exit_game():
@@ -38,22 +53,73 @@ def exit_game():
     exit()
 
 
-def cpu_move():
+def cpu_move(board, icon):
+    good_move = False
+    if icon.lower() == "x":
+        cpu_icon = "O"
+    else:
+        cpu_icon = "X"
+    while good_move == False:
+        column = random.randint(1, 3)
+        row = random.randint(1, 3)
+        if board[row - 1, column - 1] == "-":
+            board[row - 1, column - 1] = cpu_icon
+            good_move = True
+    return board
 
-    return
+
+def user_move(board, icon):
+    good_move = False
+    while good_move == False:
+        row = input("Select a row to place your move (1-3): \n")
+        row = int(row)
+        column = input("Select a column to place your move (1-3). \n")
+        column = int(column)
+        if (row < 4) and (column < 4):
+            if board[row - 1, column - 1] == "-":
+                board[row - 1, column - 1] = icon
+                good_move = True
+            else:
+                print("Choose an empty space!")
+        else:
+            print("Choose a Row and Column from 1-3!")
+    return board
 
 
-def user_move(board_key, icon):
-    pos = input("Select a cell by picking a number from the board: \n")
-    for x in board_key:
-        while x == pos:
-            x = input("That spot is taken! Pick a different move.")
-    move = board_key.index(pos)
-    board_key[move] = icon
+def display_board(board):
+    print(board)
 
 
-def display_key(board_key):
-    print(board_key)
+def check_winner(board, icon, openslots):
+    if icon.lower() == "x":
+        cpu_icon = "O"
+    else:
+        cpu_icon = "X"
+
+    for i in range(0, 3):
+        rows_win = (board[i, :] == icon).all()
+        cols_win = (board[:, i] == icon).all()
+        if rows_win or cols_win:
+            print("******* YOU WIN *******")
+            exit_game()
+        rows_win = (board[i, :] == cpu_icon).all()
+        cols_win = (board[:, i] == cpu_icon).all()
+        if rows_win or cols_win:
+            print("Loser!")
+            exit_game()
+
+    diag_win1 = (np.diag(board) == icon).all()
+    diag_win2 = (np.diag(np.fliplr(board)) == icon).all()
+
+    if diag_win1 or diag_win2:
+        print("******* YOU WIN *******")
+        exit_game()
+    diag_win1 = (np.diag(board) == cpu_icon).all()
+    diag_win2 = (np.diag(np.fliplr(board)) == cpu_icon).all()
+
+    if openslots == 0:
+        print("DRAW")
+        exit_game()
 
 
 main()
